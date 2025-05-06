902d24100d03db4c9aee25ae0f2a55b0e4c9735a
KerolosNabil7 <kerolosnabil926@gmail.com>
2025-05-06 14:34:14 +0300

    Implement the CRUD operations for the Customer Microservice

--- Customer.Microservice/Program.cs
+++ Customer.Microservice/Program.cs
@@ -1,3 +1,7 @@
+using Customer.Microservice.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        sqlOptions => sqlOptions.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName))
+);
+
+builder.Services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 
 var app = builder.Build();
 
